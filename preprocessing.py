2574be7031b74f4858c54ce9562409b69b312a4b
kevin879275 <gevin879275@gmail.com>
2021-04-20 10:09:17 +0800

    modify

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 
-root = './train'
+root = './train_image'
 
 data = []
 label = []
@@ -10,6 +10,19 @@ label = []
 max_width = 0
 max_height = 0
 i = 0
+
+label_list = {}
+
+if not os.path.isdir('data'):
+    os.mkdir('data')
+    os.mkdir('data/image')
+    os.mkdir('data/label')
+
+f = open('training data dic.txt', 'r', encoding="utf-8")
+for idx, line in enumerate(f.readlines()):
+    label_list[line] = idx
+
+
 for idx, file in enumerate(os.listdir(root)):
     img_path = root + '/' + file
     im = Image.open(img_path).convert('L')
@@ -23,10 +36,14 @@ for idx, file in enumerate(os.listdir(root)):
     # max_height = max(max_height, im.size[1])
     data.append(im_data)
     # Image.fromarray(data[-1]).show()
-    label.append(img_path[-5:-4])
+    l = img_path[-5:-4]
+    if l in label_list:
+        label.append(label_list[l])
+    else:
+        label.append(801)
     if idx % 10000 == 9999 or idx == len(os.listdir(root)) - 1:
-        np.save('train_data_' + str(i)+'.npy', data)
-        np.save('train_label_'+str(i)+'.npy', label)
+        np.save('/data/image/train_data_' + str(i)+'.npy', data)
+        np.save('/data/label/train_label_'+str(i)+'.npy', label)
         i = i + 1
         data = []
         label = []
