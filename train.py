1a24bdab658f9732a96d4b99882944006ca7a13c
kevin879275 <gevin879275@gmail.com>
2021-04-22 17:34:39 +0800

    add save model and result

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import time
 import matplotlib.pyplot as plt
 import argparse
 import torch.utils.data as data
+import json
 try:
     from tqdm import tqdm
 except ImportError:
@@ -29,8 +30,8 @@ parser.add_argument("-l", "--learning_rate", type=float, default=0.01)
 parser.add_argument("-s", "--split_rate", type=float, default=0.8)
 parser.add_argument("-r", "--resize", type=int, default=True)
 parser.add_argument("-rs", "--resize_size", type=int, default=128)
-parser.add_argument("-vb", "--validbatchsize", type=int, default=1)
-# parser.add_argument("-g", "--gpu", type=int, default=0)
+parser.add_argument("-vb", "--validbatchsize", type=int, default=64)
+parser.add_argument("-g", "--gpu", type=int, default=0)
 args = parser.parse_args()
 
 # file path
@@ -43,6 +44,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # print(device)
 
 # Hyper Parameters
+METHOD = "REGNET"
 Epoch = args.epochs
 BATCH_SIZE = args.batchsize
 lr = args.learning_rate
@@ -62,6 +64,13 @@ def load_label_dic(label_path):
 
 
 def main():
+    print("init data folder")
+
+    if not os.path.exists(str('./checkpoints')):
+        os.mkdir('checkpoints')
+    if not os.path.exists(str('./checkpoints/' + METHOD)):
+        os.mkdir('./checkpoints/' + METHOD)
+
     label_dic = load_label_dic(label_path)
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -75,10 +84,10 @@ def main():
         dataset, [train_set_size, valid_set_size])
 
     train_dataloader = DataLoader(
-        train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=False, num_workers=1)
+        train_dataset, batch_size=BATCH_SIZE, shuffle=True, pin_memory=True, num_workers=8)
 
     valid_dataloader = DataLoader(
-        valid_dataset, batch_size=valid_batch_size, pin_memory=False, num_workers=1)
+        valid_dataset, batch_size=valid_batch_size, pin_memory=True, num_workers=8)
 
     in_features = dataset[0][0].shape[0]
     # for resnet
@@ -91,14 +100,13 @@ def main():
     # model = Model(in_features=in_features).to(device)
     # summary(model, (1, resize_size, resize_size))
     loss = SmoothCrossEntropyLoss().to(device)
-
     optimizer = optim.AdamW(model.parameters(), lr=lr)
 
     print("------------------ training start -----------------")
-    total_training_loss = []
-    total_training_correct = []
-    total_valid_loss = []
-    total_valid_correct = []
+
+    result_param = {'training_loss': [], 'training_accuracy': [],
+                    'validation_loss': [], 'validation_accuracy': []}
+
     for epoch in range(Epoch):
         train_bar = tqdm(train_dataloader)
         since = time.time()
@@ -128,27 +136,34 @@ def main():
                 _, pred_class = torch.max(out.data, 1)
                 running_valid_correct += torch.sum(pred_class == label)
                 running_valid_loss += loss_val
-        total_training_loss.append(
+
+        result_param['training_loss'].append(
             running_training_loss.item() / train_set_size)
-        total_training_correct.append(running_training_correct.item() /
-                                      train_set_size)
-        total_valid_loss.append(
+        result_param['training_accuracy'].append(running_training_correct.item() /
+                                                 train_set_size)
+        result_param['validation_loss'].append(
             running_valid_loss.item() / valid_set_size)
-        total_valid_correct.append(running_valid_correct.item() /
-                                   valid_set_size)
+        result_param['validation_accuracy'].append(running_valid_correct.item() /
+                                                   valid_set_size)
+
         print("Epoch:{} Train Loss:{:.4f},  Train Accuracy:{:.4f},  Validation Loss:{:.4f},  Validation Accuracy:{:.4f}".format(
-            epoch+1, total_training_loss[-1], total_training_correct[-1], total_valid_loss[-1], total_valid_correct[-1]))
+            epoch+1, result_param['training_loss'][-1], result_param['training_accuracy'][-1], result_param['validation_loss'][-1], result_param['validation_accuracy'][-1]))
+
         now_time = time.time() - since
         print("Training time is:{:.0f}m {:.0f}s".format(
             now_time // 60, now_time % 60))
+
         train_bar.set_description(desc='[%d/%d] | Train Loss:%.4f | Train Accuracy:%.4f | Validation Loss:%.4f '
                                        '| Validation Accuracy:%.4f | Training time:%.0f' %
-                                       (epoch+1, Epoch, total_training_loss[-1], total_training_correct[-1],
-                                        total_valid_loss[-1],
-                                        total_valid_correct[-1], now_time))
-        # train_bar.set_description(desc='[%d/%d] | Loss:%.4f | Accuracy:%.4f | Save Model:%r | ' %
-        #                                (epoch, EPOCH, running_results['loss'] / running_results['batch_sizes'],
-        #                                 accuracy, save_flag))
+                                       (epoch+1, Epoch, result_param['training_loss'][-1], result_param['training_accuracy'][-1],
+                                        result_param['validation_loss'][-1],
+                                        result_param['validation_accuracy'][-1], now_time))
+
+        torch.save(model.state_dict(), str(
+            './checkpoints/' + METHOD + '/' + "EPOCH_" + str(epoch) + ".pkl"))
+        out_file = open(str(
+            './checkpoints/' + METHOD + '/' + 'result_param.json'), "w+")
+        json.dump(result_param, out_file, indent=4)
 
     plt.plot(range(1, Epoch + 1), total_training_loss)
     plt.title("loss value")
