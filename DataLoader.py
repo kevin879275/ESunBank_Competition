c4e915e4488f7562d85ab3656f9ce5db3062556b
moddent <46134191+moddent@users.noreply.github.com>
2021-05-03 23:37:06 +0800

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -4,7 +4,23 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import json
 
-
+unsuitable_data = ['10412_家.jpg',
+'18683_邦.jpg',
+'21273_研.jpg',
+'23165_學.jpg',
+'31737_遠.jpg',
+'34694_居.jpg',
+'36591_茂.jpg',
+'40121_苑.jpg',
+'43255_普.jpg',
+'4362_灣.jpg',
+'48779_灣.jpg',
+'57224_立.jpg',
+'59937_興.jpg',
+'60115_瑞.jpg',
+'62199_格.jpg',
+'62323_鈦.jpg',
+'66241_悅.jpg']
 class ChineseHandWriteDataset(Dataset):
     def __init__(self, root="", label_dic={}, transform=None, resize=True, resize_size=128):
         self.imgfile = []
@@ -15,6 +31,10 @@ class ChineseHandWriteDataset(Dataset):
         self.resize_size = resize_size
         self.label_dic = label_dic
         self.img_file = os.listdir(self.root)
+        # remove width < 10 or height < 10 image
+        for i in unsuitable_data:
+            if i in self.img_file:
+                self.img_file.remove(i)
         # for dir in os.listdir(root):
         #     dir_path = root + '/' + dir
         #     if dir == 'image':
